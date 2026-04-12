860755bf8fcbe3a1e96707be9db4089ea0de4a3d
Fuat Kocabıçak <fuat@servispro.com.tr>
2026-04-12 23:29:39 +0300

    fix: force-dynamic eklendi, prerender hatası çözüldü

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
   },
 }
 
